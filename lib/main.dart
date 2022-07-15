d1d84adf13f6687db6d91bb883aed285d3eca705
DiegoGielda <diegogielda7@gmail.com>
2022-07-15 20:29:08 -0300

    Adicionado lista de trasferências

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100,00'),
+                  subtitle: Text('1000,00'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('200,00'),
+                  subtitle: Text('2000,00'),
+                ),
+              ),
+            ],
+          ),
           appBar: AppBar(
             title: Text('Trasferência'),
           ),
